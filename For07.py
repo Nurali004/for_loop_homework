6bb7c334317defaa934a22b71c3703e530339e3f
M A C W I N <mavzurovnurali@gmail.com>
2025-05-07 01:41:01 +0500

    iiii

--- For07.py
+++ For07.py
@@ -6,4 +6,15 @@ def main(N):
     Returns:
         int: return  answer
     """
-    return
+    yigindi=0
+    for i in range(N+1):
+        if i%2!=0:
+            yigindi+=i
+
+    
+
+    return yigindi
+
+N=int(input("sonni kiriting:"))
+
+print(main(N))
